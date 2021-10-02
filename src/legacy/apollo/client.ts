260fbe412528b6e16ef0cf86de839d43a1b253c9
sam bacha <sam@manifoldfinance.com>
2021-10-02 02:48:30 -0700

    refactor(client): apollo client upgrade

--- src/legacy/apollo/client.ts
+++ src/legacy/apollo/client.ts
@@ -1,12 +1,12 @@
-import { ApolloClient } from 'apollo-client';
-import { InMemoryCache } from 'apollo-cache-inmemory';
-import { HttpLink } from 'apollo-link-http';
+
+import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client';
 import fetch from 'node-fetch';
 
 export default new ApolloClient({
   link: new HttpLink({
     fetch,
-    uri: 'https://api.thegraph.com/subgraphs/name/graphprotocol/uniswap',
+    uri: 'https://api.thegraph.com/subgraphs/name/sushiswap/exchange/graphql',
   }),
   cache: new InMemoryCache(),
 });
+
--- src/swap/blocks/client.ts
+++ src/swap/blocks/client.ts
@@ -1,6 +1,4 @@
-import { InMemoryCache } from 'apollo-cache-inmemory';
-import { ApolloClient } from 'apollo-client';
-import { HttpLink } from 'apollo-link-http';
+import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client';
 import fetch from 'node-fetch';
 
 export const blockClient = new ApolloClient({
